29895a62cb3ef82e03dc4378968a0e41a7b121b6
wonAdam <ehdgus5500@gmail.com>
2021-02-23 18:13:57 +0900

    3분만에 만든 스탑워치... 윈폼 짱 편해...

---
+++ StopWatch/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿
+namespace StopWatch
+{
+    partial class frmMainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.txtStop = new System.Windows.Forms.TextBox();
+            this.txtElapsed = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(21, 17);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(21, 46);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "S&top";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(21, 75);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "&Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(212, 19);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(100, 20);
+            this.txtStart.TabIndex = 3;
+            // 
+            // txtStop
+            // 
+            this.txtStop.Location = new System.Drawing.Point(212, 48);
+            this.txtStop.Name = "txtStop";
+            this.txtStop.Size = new System.Drawing.Size(100, 20);
+            this.txtStop.TabIndex = 4;
+            // 
+            // txtElapsed
+            // 
+            this.txtElapsed.Location = new System.Drawing.Point(212, 74);
+            this.txtElapsed.Name = "txtElapsed";
+            this.txtElapsed.Size = new System.Drawing.Size(100, 20);
+            this.txtElapsed.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Enabled = false;
+            this.label1.Location = new System.Drawing.Point(151, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Start Time";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "End Time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(135, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Elapsed Time";
+            // 
+            // frmMainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 117);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtElapsed);
+            this.Controls.Add(this.txtStop);
+            this.Controls.Add(this.txtStart);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Stop Watch";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtStart;
+        private System.Windows.Forms.TextBox txtStop;
+        private System.Windows.Forms.TextBox txtElapsed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ StopWatch/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopWatch
+{
+    public partial class frmMainForm : Form
+    {
+        public frmMainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        DateTime start;
+        DateTime stop;
+        TimeSpan elapsed;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtStop.Text = String.Empty;
+            txtElapsed.Text = String.Empty;
+
+            start = DateTime.Now;
+            txtStart.Text = start.ToLongTimeString();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (start != null)
+            {
+                stop = DateTime.Now;
+                txtStop.Text = stop.ToLongTimeString();
+                elapsed = stop - start;
+                txtElapsed.Text = elapsed.Seconds.ToString();
+            }
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
